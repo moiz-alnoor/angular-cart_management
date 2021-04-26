d1178100696d92422e1a8e408743ae8aa4ff6240
Mr-moiz <mmoiz.aalnoor@gmail.com.com>
2021-04-27 00:22:38 +0400

    details added

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -31,8 +31,10 @@ exist:any
 
 //  all this key are available
 
-//apiKey = 'e2fc984542944254a3ff7b6fe1e553ab'
-apiKey = '3adcaf7fdc8f400f84ae38d0eafa4a75'
+apiKey = 'e2fc984542944254a3ff7b6fe1e553ab'
+//apiKey = '3adcaf7fdc8f400f84ae38d0eafa4a75'
+
+// not use for dev
 //apiKey = 'cbac52df41214743a4dabcacf89b4226'
 //apiKey = 'c63e3cb004bf4b62a30b3332ed3895ae'
 
