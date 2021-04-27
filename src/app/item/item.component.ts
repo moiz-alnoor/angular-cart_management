988179e5df0eb28a17452a2377ec15514a656ded
Mr-moiz <mmoiz.aalnoor@gmail.com.com>
2021-04-27 13:39:55 +0400

    adding new apikey

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -32,8 +32,8 @@ export class ItemComponent implements OnInit {
 
 
   // apiKey = 'e2fc984542944254a3ff7b6fe1e553ab';
-  apiKey = '3adcaf7fdc8f400f84ae38d0eafa4a75';
-  //apiKey = 'cbac52df41214743a4dabcacf89b4226'
+  //apiKey = '3adcaf7fdc8f400f84ae38d0eafa4a75';
+  apiKey = 'cbac52df41214743a4dabcacf89b4226'
   //apiKey = 'c63e3cb004bf4b62a30b3332ed3895ae'
 
   constructor(private ProductsServiceService: ProductsServiceService) {}
